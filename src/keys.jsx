4d1018aa7d547901c881057247ee6f0c337fd0fa
long104 <longpantorn@gmail.com>
2024-02-05 16:57:45 +0700

    change

--- src/keys.jsx
+++ src/keys.jsx
@@ -1,6 +1,6 @@
 const keys = {
-	API_KEY: "d6390fefcc810007e3643e31d4cc0ff3",
-	BASE_URL: "https://api.openweathermap.org/data/2.5/",
+	VITE_API_KEY: "d6390fefcc810007e3643e31d4cc0ff3",
+	VITE_BASE_URL: "https://api.openweathermap.org/data/2.5/",
 };
 
 export default keys;
